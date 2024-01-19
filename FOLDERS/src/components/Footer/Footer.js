f0bf24fa892eb3be428c34d108decb47a0625dd5
shadrackbenjamin <131531398+shadrackbenjamin@users.noreply.github.com>
2024-01-19 23:32:25 +0000

    Add files via upload

---
+++ FOLDERS/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ❤️ by 99Angels
+      
+    </div>
+  );
+};
+
+export default Footer;
